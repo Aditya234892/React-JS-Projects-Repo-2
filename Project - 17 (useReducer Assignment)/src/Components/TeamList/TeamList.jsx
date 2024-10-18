e79ba50d58e5c5104c93691711bdf2399c70f926
Aditya <adityadevansh2002@gmail.com>
2024-10-18 13:12:24 +0530

    added designs to buttons

--- Project - 17 (useReducer Assignment)/src/Components/TeamList/TeamList.jsx
+++ Project - 17 (useReducer Assignment)/src/Components/TeamList/TeamList.jsx
@@ -4,12 +4,12 @@ function TeamList(props) {
   return (
     <div className="teamList-card flex flex-col gap-2 p-4 mt-3 mb-3 w-2/5 rounded-lg shadow-md bg-green-400">
       <h1 className="text-2xl text-center font-extrabold">Team List</h1>
-      <div className="w-full bg-gray-200 flex justify-between p-3 rounded-md"> 
-        <button onClick={() => props.dispatch({ type: "Calculate_average_age" })}>
+      <div className="w-full bg-gray-200 flex justify-between items-center p-3 rounded-md"> 
+        <button onClick={() => props.dispatch({ type: "Calculate_average_age" })} className="bg-blue-400 py-1 px-2 rounded-md shadow-md shadow-orange-500 font-semibold active:scale-95">
           Calculate Average Age
         </button>
         <p>Average age: {props.averageAge.toFixed(2)}</p>
-        <button onClick={() => props.dispatch({ type: "Sort_by_age" })}>Sort By Age</button>
+        <button onClick={() => props.dispatch({ type: "Sort_by_age" })} className="bg-orange-400 py-1 px-2 rounded-md shadow-md shadow-blue-500 font-semibold active:scale-95">Sort By Age</button>
       </div>
       {props.data.map((employee) => (
         <div
